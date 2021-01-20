cbc8e36f8aae18baebf26d51d9ca0492e6d419e8
xebec19 <rohanthakur-doof@gmail.com>
2021-01-20 23:40:39 +0530

    save

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -4,14 +4,30 @@ class Chat extends React.Component{
 	constructor(){
 		super();
 		this.state = {
-			message: ''
+			message: '',
+			data:[]
 		}
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}  //constructor ends here
 	handleChange(e){
 		this.setState({[e.target.name]: e.target.value});
+	}  //handleChange ends here
+
+	componentDidMount(){
+		firebaseDb.child('messages').on('value',snapshot => {
+			if(snapshot.val()!=null){
+				let temp = []
+			/*this.setState({data: ...snapshot.val()});*/
+				snapshot.forEach((item) => {
+					temp.push(item);
+				})
+				console.log('temp:',temp)
+				this.setState({data:temp})
+			}
+		})
 	}
+
 	handleSubmit(e){
 		firebaseDb.child('messages').push(
 			this.state.message,
@@ -20,9 +36,13 @@ class Chat extends React.Component{
 			}
 		)
 	}  //handleSubmit ends here
+
 	render(){
 		return(
 			<div>
+				<div>
+					{console.log('Value of data',this.state.data[0])}
+				</div>
 				<input type='text' name='message' onChange={(e) => this.handleChange(e)} />
 				<button onClick={() => this.handleSubmit()}>Submit</button>
 			</div>
