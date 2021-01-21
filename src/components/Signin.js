a7edadb71de882399f7f02d8052613fdbe1f4c0d
xebec19 <rohanthakur-doof@gmail.com>
2021-01-21 13:58:32 +0530

    save

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -3,44 +3,44 @@ import firebase from 'firebase';
 import 'firebase/auth';
 
 class Signin extends React.Component{
-	
-	constructor(props){
-		super(props);
-		this.state={
-		authenticated: false,
-		loading: true
-		}  //state ends here
-		this.signInWithGoogle = this.signInWithGoogle.bind(this);
-		}  //constructor ends here
 
-		componentDidMount(){
-		firebase.auth().onAuthStateChanged((user) => {
-		if(user){
-		this.setState({
-		authenticated:true,
-		loading:false
-		});
-		this.props.setAuth(true)
-		}else{
-		this.setState({
-		authenticated:false,
-		loading:false
-		});
-		}
-		})
-		}  //componentDidMount ends here
+constructor(props){
+super(props);
+this.state={
+authenticated: false,
+loading: true
+}  //state ends here
+this.signInWithGoogle = this.signInWithGoogle.bind(this);
+}  //constructor ends here
 
-		signInWithGoogle() {
-			const provider = new firebase.auth().GoogleAuthProvider();
-			return firebase.auth().signInWithPopup(provider);
-		  }
-	render(){
-		return(
-			<div>
-			<button onClick={() => this.signInWithGoogle}>Sign in</button>
-			</div>
-			)
-	}
+componentDidMount(){
+firebase.auth().onAuthStateChanged((user) => {
+if(user){
+this.setState({
+authenticated:true,
+loading:false
+});
+this.props.setAuth(true)
+}else{
+this.setState({
+authenticated:false,
+loading:false
+});
+}
+})
+}  //componentDidMount ends here
+
+signInWithGoogle() {
+const provider = new firebase.auth().GoogleAuthProvider();
+return firebase.auth().signInWithPopup(provider);
+}
+render(){
+return(
+<div>
+<button onClick={() => this.signInWithGoogle}>Sign in</button>
+</div>
+)
+}
 }
 
 export default Signin;
